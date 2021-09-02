2b10c207f09d9117e41b0e4655d3eab49548df7e
yajunli01 <jason.li@igg.com>
2021-09-02 10:04:07 +0800

    init first window

---
+++ myOpenGL/envTest.cpp
@@ -0,0 +1,46 @@
+#include <glad/glad.h>
+#include <glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* win, int w, int h);
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* win = glfwCreateWindow(800, 600, u8"Ñ§Ï°OpenGL", NULL, NULL);
+	if (win == NULL)
+	{
+		std::cout << "Ê§°Ü´´½¨ GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(win);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(win, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(win))
+	{
+		glfwSwapBuffers(win);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* win, int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
