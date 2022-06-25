241a5bead1bd4c229798fc9ed908ac493dd46c00
kingmax <kingmax_res@163.com>
2022-06-25 21:27:48 +0800

    draw 2 triangles => one rectangle use EBO

--- myOpenGL/main.cpp
+++ myOpenGL/main.cpp
@@ -9,18 +9,43 @@ using namespace std;
 
 #include "shader.h"
 
+#pragma region Data
 // Vertex Data
-// Vertex_Array_Object
 float vertices[] = {
 	-0.5f, -0.5f, 0.0f,
 	0.5f, -0.5f, 0.0f,
 	0.0f, 0.5f, 0.0f
 };
 
+// Vertex Array Object (VAO, Array of VBO)
+unsigned int VAO;
+
+// Vertex Buffer Object (VBO)
+unsigned int VBO;
+
+// Shader File
 const string vsShaderFile = "first.vs";
 const string psShaderFile = "first.ps";
 
+// Element Buffer Object (EBO)
+float rectangle_vertices[] = {
+	0.5f, 0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f,
+	-0.5f, 0.5f, 0.0f
+};
 
+unsigned int rectangle_indices[] = {
+	0, 1, 3, // 第一个三角形，顺时针
+	1, 2, 3  // 第二个三角形，顺时针
+};
+
+unsigned int EBO;
+#pragma endregion Data
+
+
+
+#pragma region functions
 void framebuffer_size_callback(GLFWwindow* win, int w, int h)
 {
 	cout << "size changed: " << w << " x " << h << endl;
@@ -34,6 +59,7 @@ void processInput(GLFWwindow* win)
 		glfwSetWindowShouldClose(win, true);
 	}
 }
+#pragma endregion functions
 
 
 int main()
@@ -63,12 +89,12 @@ int main()
 
 	// using Vertex Array Object (VAO) (存放VBO的数组，方便管理大量物体顶点配置数据)
 	// 必须放在VBO前面
-	unsigned int VAO;
+	// unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
 	//读入顶点数据，选产生一个VBO对象 (Vertex Buffer Object), 然后绑定到GL_ARRAY_BUFFER, 最后将实际顶点数据复制到绑定的Buffer
-	unsigned int VBO;
+	// unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -142,6 +168,19 @@ void main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// 使用 EBO 重画2个三角形组成一个矩形 (完整流程，覆盖了以上画单个三角形的一系列设置)
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle_vertices), rectangle_vertices, GL_STATIC_DRAW);
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(rectangle_indices), rectangle_indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// 线框模式
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 	// 渲染循环
 	while (!glfwWindowShouldClose(win))
 	{
@@ -151,12 +190,21 @@ void main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		#pragma region draw one triangle
 		// use ShaderProgramObject
-		glUseProgram(shaderProgram);
+		//glUseProgram(shaderProgram);
 		// 绘制数据 (通过VAO间接绑定了VBO, 而VBO已由顶点属性配置)
-		glBindVertexArray(VAO);
+		//glBindVertexArray(VAO);
 		// 使用当前激活的shader绘制图元
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		#pragma endregion draw one triangle
+
+		// using EBO draw Rectangle (2 triangles)
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(win);
 		glfwPollEvents();
